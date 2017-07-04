21dfeee648d184ac7f43adb80f8353ed39b2f0e3
ELIZABETHNJ <elizabethnj1996@gmail.com>
2017-07-04 22:16:20 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,21 @@
+# even
+import java.io.*;
+import java.util.*;
+public class Even
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner (System.in);
+    int n1,n2;
+    System.out.println("Enter the range");
+    n1=sc.nextInt();
+    n2=sc.nextInt();
+    for(int i=n1; i<=n2; i++)
+    {
+      if(i%2==0)
+      {
+        System.out.println(i);
+      }
+    }
+  }
+  }
